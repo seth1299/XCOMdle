5a4b2492a14b6e5b1e3d2818bb0278d72ce59ebe
Seth Grimes <seth.grimes1999@gmail.com>
2024-10-08 11:34:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import sqlite3
+import os
 
 '''
 Access-Control-Allow-Origin: *
@@ -85,5 +86,6 @@ def search():
         return jsonify(abilities)
     return jsonify([])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
